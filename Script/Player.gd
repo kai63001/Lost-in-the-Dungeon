3f4f8f3c5be20ede83060c4714aff3565d85096b
kai63001 <animehq5@gmail.com>
2021-08-26 18:04:33 +0700

    update

--- Script/Player.gd
+++ Script/Player.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 export var speed = 100
 
 var velocity = Vector2.ZERO
+onready var Dialog = $dialog
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -10,6 +11,7 @@ func _physics_process(delta):
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
 	if input_vector != Vector2.ZERO:
+		callDialog()
 		velocity = input_vector * speed
 		$AnimatedSprite.play("run")
 		$AnimatedSprite.flip_h = velocity.x < 0
@@ -19,3 +21,8 @@ func _physics_process(delta):
 		
 	
 	move_and_collide(velocity * delta)
+	
+func callDialog():
+	Dialog.visible = true
+	Dialog.position = Vector2($AnimatedSprite.position.x + 50,$AnimatedSprite.position.y - 70)
+	$dialog/Label.text = "Where the fuck"
