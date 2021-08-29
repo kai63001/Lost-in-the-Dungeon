ad4e848498be2668f986c174acac3581bab0fe27
kai63001 <animehq5@gmail.com>
2021-08-29 19:42:32 +0700

    update

---
+++ Script/perps/Trap.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+var run = false
+export var howManyTrap = 1
+var colition
+onready var trap = $TrapAnimation
+onready var boxColition = $CollisionShape2D
+
+
+func _ready():
+	boxColition.scale()
+	pass # Replace with function body.
+
+
+func _on_Trap_body_entered(body):
+	if(body.is_in_group("player") && run == false):
+		$TrapAnimation.play("trap")
+		print("1")
+		run = true
+
+
+func _on_Trap_body_exited(body):
+	if(body.is_in_group("player")):
+		$TrapAnimation.stop()
